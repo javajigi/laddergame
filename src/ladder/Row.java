fdb3e90e03c74c795d3e683c336fc5b3ab49a46a
javajigi <javajigi@gmail.com>
2014-10-27 18:44:43 +0900

    Row 객체의 moveRow 메서드 리팩토링 지금까지 구현 과정은 http://youtu.be/z3OAEFSlzsM 참고

--- src/ladder/Row.java
+++ src/ladder/Row.java
@@ -2,27 +2,37 @@ package ladder;
 
 public class Row {
 	int[] persons;
-	
+
 	public Row(int noOfPersons) {
 		persons = new int[noOfPersons];
 	}
-	
+
 	void drawLine(int startPosition) {
 		persons[startPosition] = 1;
 		persons[startPosition + 1] = 1;
 	}
-	
+
 	int moveRow(int nthOfPerson) {
-		if (persons[nthOfPerson] == 0) {
+		if (isNotLine(nthOfPerson)) {
 			return nthOfPerson;
 		}
 
-		if (nthOfPerson - 1 >= 0) {
-			if (persons[nthOfPerson - 1] == 1) {
-				return nthOfPerson - 1;
-			}
+		if (isNotLeftEnd(nthOfPerson) && canLeft(nthOfPerson)) {
+			return nthOfPerson - 1;
 		}
 
 		return nthOfPerson + 1;
+	}
+
+	private boolean canLeft(int nthOfPerson) {
+		return persons[nthOfPerson - 1] == 1;
+	}
+
+	private boolean isNotLeftEnd(int nthOfPerson) {
+		return nthOfPerson - 1 >= 0;
+	}
+
+	private boolean isNotLine(int nthOfPerson) {
+		return persons[nthOfPerson] == 0;
 	}
 }
