84e9c9c7cfacbc804ce2a7e492b71308f76b2856
javajigi <javajigi@gmail.com>
2014-11-03 11:25:17 +0900

    static final을 활용해 상수 값 생성 및 중복 제거

--- src/ladder/Row.java
+++ src/ladder/Row.java
@@ -1,6 +1,10 @@
 package ladder;
 
 public class Row {
+	private static final int LEFT_DIRECTION = -1;
+	private static final int CENTER_DIRECTION = 0;
+	private static final int RIGHT_DIRECTION = 1;
+	
 	int[] persons;
 
 	public Row(int noOfPersons) {
@@ -8,8 +12,8 @@ public class Row {
 	}
 
 	void drawLine(int startPosition) {
-		persons[startPosition] = 1;
-		persons[startPosition + 1] = -1;
+		persons[startPosition] = RIGHT_DIRECTION;
+		persons[startPosition + 1] = LEFT_DIRECTION;
 	}
 
 	int move(int nthOfPerson) {
@@ -25,10 +29,10 @@ public class Row {
 	}
 
 	private boolean isRightDirection(int nthOfPerson) {
-		return persons[nthOfPerson] == 1;
+		return persons[nthOfPerson] == RIGHT_DIRECTION;
 	}
 
 	private boolean isNotLine(int nthOfPerson) {
-		return persons[nthOfPerson] == 0;
+		return persons[nthOfPerson] == CENTER_DIRECTION;
 	}
 }
